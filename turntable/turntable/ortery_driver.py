5230135eabddcd05d6426e6a65452ff7f56e588f
Floris Erich <floris.erich@aist.go.jp>
2021-07-30 17:41:23 +0900

    Added get_command_desc service

---
+++ turntable/turntable/ortery_driver.py
@@ -0,0 +1,82 @@
+import re
+import subprocess
+from collections import namedtuple
+
+
+def rwo(command):
+    """Wrap subprocess.run to always capture output."""
+    return subprocess.run(command, capture_output=True)
+
+
+def get_device_count():
+    """Get the number of devices connected to this PC."""
+    proc = rwo("OTADCommand.exe get_device_count")
+    m = re.search('^([0-9]+)\\r\\n$', proc.stdout.decode("utf-8"))
+    return m.group(1)
+
+
+class InvalidIdException(Exception):
+    """Exception thrown when an invalid ID was passed."""
+    def __init__(self, device_id):
+        Exception.__init__(self)
+        self.device_id = device_id
+
+
+class DeviceInfo():
+    """Information of a device."""
+    def __init__(self, product_name, device_id):
+        self.product_name = product_name
+        self.device_id = device_id
+
+
+def get_device_info(device_id):
+    proc = rwo(f"OTADCommand.exe get_device_info {device_id}")
+    e = 'get_device_info :  command exec fail ( error code : 0x0040001)\r\n'
+    output = proc.stdout.decode('utf-8') 
+    if output == e:
+        raise InvalidIdException(device_id)
+
+    s = '^Product Name : ([A-Za-z0-9 ]+)\\r\\nDevice ID : ([0-9]+)\\r\\n'
+    m = re.search(s, output)
+    return DeviceInfo(m.group(1), m.group(2))
+
+
+Command = namedtuple("Command", "name value description")
+command_dict = {
+    12801: Command("otadDEVICE_COMMAND_CABLERLEASE_OFF",
+                   12801,
+                   "Shutter Release OFF"),
+    12802: Command("otadDEVICE_COMMAND_CABLERLEASE_HALFWAY",
+                   12802,
+                   "Shutter Release Halfway (Focus)"),
+    12803: Command("otadDEVICE_COMMAND_CABLERLEASE_COMPLETELY",
+                   12803,
+                   "Shutter Release Completely (Snap)"),
+    13057: Command("otadDEVICE_COMMAND_TURNTABLE_STOP",
+                   13057,
+                   "Stop the turntable"),
+    13058: Command("otadDEVICE_COMMAND_TURNTABLE_RELEASE",
+                   13058,
+                   "Release the motor of turntable"),
+    16641: Command("otadDEVICE_PROPERTY_TURNTABLE_STATE",
+                   16641,
+                   "State of turntable")
+    }
+
+
+
+class UnknownCommand():
+    pass
+
+
+def get_command_desc(device_id):
+    """Get a list of commands supported by the device."""
+    proc = rwo(f"OTADCommand.exe get_command_desc {device_id}")
+    e = b'get_command_desc :  command exec fail ( error code : 0x0040001)\r\n'
+    output = proc.stdout.decode('utf-8')
+    if output == e:
+        raise InvalidIdException(device_id)
+
+    command_ids = re.findall("([0-9]+)\r\n", output)
+    return [command_dict.get(int(command_id), UnknownCommand()) 
+            for command_id in command_ids]
--- turntable/turntable/turntable_node.py
+++ turntable/turntable/turntable_node.py
@@ -1,12 +1,17 @@
-import re
-import subprocess
 import rclpy
+import ortery_driver as driver
 from rclpy.node import Node
-from turntable_srvs.srv import GetDeviceCount
+from turntable_srvs.msg import CommandDesc
+from turntable_srvs.srv import GetDeviceCount, GetDeviceInfo, GetCommandDescs
 
 
-def rwo(command):
-    return subprocess.run(command, capture_output=True)
+def map_ortery_command_desc_to_ros_type(ocd):
+    desc = CommandDesc()
+    desc.name = ocd.name
+    desc.value = ocd.value
+    desc.description = ocd.description
+    return desc
+
 
 class TurntableNode(Node):
     def __init__(self):
@@ -18,29 +23,37 @@ class TurntableNode(Node):
                                     GetDeviceInfo,
                                     'get_device_info',
                                     self.get_device_info_callback)
+        self.get_command_desc =  self.create_service(
+                                    GetCommandDesc,
+                                    'get_command_desc',
+                                    self.get_command_desc_callback)
         
     def get_device_count_callback(self, request, response):
-        proc = rwo("OTADCommand.exe get_device_count")
-        m = re.search('^([0-9]+)\\r\\n$', proc.stdout.decode("utf-8"))
-        response.count = m.group(1)
+        response.count = driver.get_device_count()
         return response
 
-    def get_device_info(self, request, response):
-        proc = rwo(f"OTADCommand.exe get_device_info {request.id}")
-        e = 'get_device_info :  command exec fail ( error code : 0x0040001)\r\n'
-        if proc.stdout.decode('utf-8') == e:
+    def get_device_info_callback(self, request, response):
+        try:
+            device_info = driver.get_device_info(request.id)
+            response.product_name = device_info.product_name
+            response.device_id = device_info.device_id
+            response.success = True
+        except InvalidIdException:
             response.success = False
-            return response
 
-        s = '^Product Name : ([A-Za-z0-9 ]+)\\r\\nDevice ID : ([0-9]+)\\r\\n'
-        m = re.search(s, proc.stdout.decode('utf-8'))
-        response.product_name = m.group(1)
-        response.device_id = m.group(2)
-        response.success = True
         return response
 
-                
-                
+    def get_command_desc_callback(self, request, response):
+        try:
+            command_descs = driver.get_command_desc(request.id)
+            response.command_descs = [
+                map_ortery_command_desc_to_ros_type(command_desc)
+                for command_desc in command_descs]
+            response.success = True
+        except InvalidIdException:
+            response.success = False
+        return response
+
 
 def main(args=None):
     rclpy.init(args=args)
